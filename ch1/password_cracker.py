1261ed662d4875f47cae04f114ea895d062ba8a8
Huberdoggy <huberdoggy@gmail.com>
2021-10-08 13:10:09 -0500

    updated notes for sha512 prog, commented out portion of prior prog since my new one works it's irrelevant

--- ch1/password_cracker.py
+++ ch1/password_cracker.py
@@ -19,29 +19,30 @@ def test_pass(crypt_pass, account_name):
             print "[-] Password NOT found.\n"
             return
     else:  # We are targeting the 'root' user pass hash
-        salt = crypt_pass[:13]  # changing the salt to correspond to 13 chars in field 1
-        hash_obj_5 = hashlib.sha512()
-        hash_obj_5.update(salt.encode())
-        sha_512_hash = hash_obj_5.hexdigest()
-        try:  # I need a REALLY big wordlist ....
-            url_pt1 = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/"
-            url_pt2 = "Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"
-            passwd_lst = urlopen(url_pt1 + url_pt2).read().decode('utf-8')
-            for password in passwd_lst.split('\n'):
-                password = password.encode('utf-8')
-                guess = hashlib.sha512(bytes(password)).hexdigest()
-                # password = password.encode('utf-8') # Ensure each individual passwd is passed as UTF-8 NOT unicode
-                # crypt_word = crypt.crypt(password, "$6${}".format(salt))
-                # This will force SHA512 hashing (i.e $6$ shorthand)
-                # print crypt_word
-                # guess = crypt_word[3:16] # This cuts the newly generated hash between the '$6$' and trailing '$'
-                if guess == sha_512_hash:
-                    print "[+] Found password: " + password
-                    break
-                else:
-                    print "[-] No match " + str(sha_512_hash) + " against " + str(guess)
-        except Exception as e:
-            print e
+        pass
+        # salt = crypt_pass[:13]  # changing the salt to correspond to 13 chars in field 1
+        # hash_obj_5 = hashlib.sha512()
+        # hash_obj_5.update(salt.encode())
+        # sha_512_hash = hash_obj_5.hexdigest()
+        # try:  # I need a REALLY big wordlist ....
+        #     url_pt1 = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/"
+        #     url_pt2 = "Passwords/Common-Credentials/10-million-password-list-top-1000000.txt"
+        #     passwd_lst = urlopen(url_pt1 + url_pt2).read().decode('utf-8')
+        #     for password in passwd_lst.split('\n'):
+        #         password = password.encode('utf-8')
+        #         guess = hashlib.sha512(bytes(password)).hexdigest()
+        #         # password = password.encode('utf-8') # Ensure each individual passwd is passed as UTF-8 NOT unicode
+        #         # crypt_word = crypt.crypt(password, "$6${}".format(salt))
+        #         # This will force SHA512 hashing (i.e $6$ shorthand)
+        #         # print crypt_word
+        #         # guess = crypt_word[3:16] # This cuts the newly generated hash between the '$6$' and trailing '$'
+        #         if guess == sha_512_hash:
+        #             print "[+] Found password: " + password
+        #             break
+        #         else:
+        #             print "[-] No match " + str(sha_512_hash) + " against " + str(guess)
+        # except Exception as e:
+        #     print e
 
 
 with open('fake_shadow_file.txt') as pass_file:
--- ch1/sha512_test.py
+++ ch1/sha512_test.py
@@ -8,8 +8,9 @@ def crack_sha(url, salt, crypt_pass):
     try:
          passwd_lst = urlopen(url).read().decode('utf-8')
          for word in passwd_lst.split('\n'):
+             # Default SHA-512 uses 5,000 rounds according to docs
              hash_word = sha512_crypt.encrypt(word, salt=salt, rounds=5000)
-             guess = hash_word[12:]
+             guess = hash_word[12:] # I want the portion after the $6$<salt>$ prefix to compare against target password
              if guess == crypt_pass:
                  print "[+] Found password: " + word
                  return
@@ -18,22 +19,26 @@ def crack_sha(url, salt, crypt_pass):
     except Exception as e:
         print e
 
+# For testing purposes/simplicity, I'm hardcoding the encrypted portion rather than cutting it out of shadow
 _crypt_pass = "NyXj0YofkK14MpRwFHvXQW0yvUid.slJtgxHE2EuQqgD74S/GaGGs5VCnqeC.bS0MzTf/EFS3uspQMNeepIAc."
 prefix = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/"
 url_lst = [
     prefix + "Common-Credentials/10-million-password-list-top-1000000.txt",
+    # I was originally going to run against multiple url lists, but after success using this one I decided
+    # Keep it as is for now
 ]
 
-regex = re.compile("(root\W)\s([a-zA-Z0-9]{8,}\W)")
+regex = re.compile("(root\W)\s([a-zA-Z0-9]{8,}\W)") # This will pull the username and salt out of shadow
 with open ('fake_shadow_file.txt', 'r') as shadow_file:
     for expression in shadow_file.readlines():
-        if re.findall(regex, expression):
+        if re.findall(regex, expression): # Just making sure my regex skills are on point...
             target_line = re.match(regex, expression)
-            _user, _salt = target_line.group(1).strip(':'), target_line.group(2).strip(':')
+            _user, _salt = target_line.group(1).strip(':'), target_line.group(2).strip(':') # Split those 2 fields
             print "[*] Cracking password for the " + _user.upper() + " account...\n"
             sleep(3)
-            for item in url_lst:
+            for item in url_lst: # See comment above for 'url_lst'
                 _url = item
                 crack_sha(_url, _salt, _crypt_pass)
         else:
             print "[-] Skipping undesired account..."
+            # I already cracked this account in my other program using default crypt.method so I don't care
